8df11caf84f018f1a2a3ff662c1fa55f2f645bc6
sssssungs <sssssungs@gmail.com>
2019-07-08 17:52:23 +0900

    템플릿 수정 (주석추가 등)

--- src/containers/login/RegisterContainer.js
+++ src/containers/login/RegisterContainer.js
@@ -236,10 +236,11 @@ class RegisterContainer extends Component {
 }
  
 
-// 컴포넌트에 리덕스 스토어를 연동해줄 때에는 connect 함수 사용
+// 컴포넌트에 리덕스 연결시 아래 형태 connect - state - dispatch 사용
 export default connect(
     (state) => ({
     }), (dispatch) => ({
+        //현재 component에서 사용하기 위해 reducer에서 가져온 함수를 할당하는 부분
         RegisterActions: bindActionCreators(registerActions, dispatch),
     })
 )(RegisterContainer);
